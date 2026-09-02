2dc980b7902011821a066ec89e1f2583c9eb81d8
m4c4c0-spec <venegaspoveda.m@gmail.com>
2026-09-02 12:04:09 -0400

    se corrije el readme para eliminar detalles tecnicos que se implementaran mas adelante

--- cowork/static/cowork/js/404.js
+++ cowork/static/cowork/js/404.js
@@ -1,10 +1,8 @@
 const backLink = document.querySelector('[data-back-link]');
 
-// Keep the home URL as a safe fallback when there is no previous local page.
 if (backLink && document.referrer) {
     const previousPage = new URL(document.referrer);
 
-    // Never send the visitor back to an external site from this control.
     if (previousPage.origin === window.location.origin) {
         backLink.addEventListener('click', (event) => {
             event.preventDefault();
